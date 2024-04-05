0fba7d213a453811fba42e735eba958466e91e95
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-05 13:37:29 +0300

    Added CurrencyConvertor Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-const App = () => {
-  return <div>App</div>;
-};
+import CurrencyConvertor from "./components/CurrencyConvertor";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <div className="container">
+        <CurrencyConvertor />
+      </div>
+    </div>
+  );
+}
 
 export default App;
